957be406763513941fe4409d029942947ae8546b
saishanmukh3 <SBommasani01@indianatech.net>
2024-11-13 18:51:42 -0500

    commited

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Profile from './pages/profile/Profile';
 
 function App() {
   return (
-    <div className="App bg-rose-400	  w-full h-screen" >
+    <div className="App bg-white	  w-full h-screen" >
       <Navbar />
       <Routes>
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';  // Import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter>  {/* Wrap the App component in BrowserRouter */}
     <App />
   </BrowserRouter>
 );
-
