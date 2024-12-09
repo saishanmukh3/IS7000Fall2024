cb3e814b80f200902e74bd84f86a23a6d3b7dd1e
saishanmukh3 <SBommasani01@indianatech.net>
2024-12-09 15:11:26 -0500

    commit

--- src/pages/admin/batch/Components/Subscriptions/ViewSubscription.js
+++ src/pages/admin/batch/Components/Subscriptions/ViewSubscription.js
@@ -21,7 +21,7 @@ const ViewSubscription = ({ updatingData, close }) => {
     <div className="flex flex-col gap-3">
       {updatingData && Object?.keys(updatingData)?.length > 0 && (
         <>
-          <div className=" text-xl font-bold bg-themeColor text-white p-3">View Subscription details</div>
+          <div className=" text-xl font-bold bg-themeColor text-white p-3"> Subscription details</div>
           <div className="p-2">
             <div className="flex flex-col">
               <div className="font-bold text-lg">Subscription Details</div>
